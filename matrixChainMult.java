0331e2fe20861489e6c14c1bb7865c95432c5619
lakshaynanda <lakshay.nanda08@gmail.com>
2020-04-25 16:28:57 +0530

    Updated MCM

--- matrixChainMult.java
+++ matrixChainMult.java
@@ -2,7 +2,7 @@ import java.util.*;
 
 public class matrixChainMult {
 
-    public static int matrixChainMultiplication(int[] dims, int si, int ei, int dp[][]) {
+    public static int matrixChainMultiplicationMem(int[] dims, int si, int ei, int dp[][]) {
         if (ei - si == 1) {
             return 0;
         }
@@ -11,8 +11,8 @@ public class matrixChainMult {
         }
         int omin = Integer.MAX_VALUE;
         for (int cbp = si + 1; cbp <= ei - 1; cbp++) {
-            int minCostPart1 = matrixChainMultiplication(dims, si, cbp, dp);
-            int minCostPart2 = matrixChainMultiplication(dims, cbp, ei, dp);
+            int minCostPart1 = matrixChainMultiplicationMem(dims, si, cbp, dp);
+            int minCostPart2 = matrixChainMultiplicationMem(dims, cbp, ei, dp);
             int pc = dims[si] * dims[ei] * dims[cbp];
 
             int totalCost = minCostPart1 + minCostPart2 + pc;
@@ -24,10 +24,48 @@ public class matrixChainMult {
         dp[si][ei]=omin;
         return omin;
     }
+    
+    public static int matrixChainMultiplicationTab(int[] dims)
+    {
+        int [][]mcm=new int[dims.length][dims.length];
+        int min=Integer.MAX_VALUE;
+        for(int gap=1;gap<dims.length;gap++)
+        {
+            int i=0;int j=i+gap;
 
+            while(j<dims.length)
+            {
+                if(gap==1)
+                {
+                    mcm[i][j]=0;
+                }
+                else
+                {
+                    for(int cp=i+1;cp<=j-1;cp++)
+                    {
+                        int fp=mcm[i][cp];
+                        int sp=mcm[cp][j];
+
+                        int factor=dims[i]*dims[j]*dims[cp];
+
+                        int totalCost=fp+sp+factor;
+                        if(totalCost<min)
+                        {
+                            min=totalCost;
+                        }
+                        mcm[i][j]=min;
+                    }
+                }
+                i++;
+                j++;
+            }
+        }
+        return mcm[0][dims.length-1];
+        
+    }
     public static void main(String args[]) {
         int dims[] = { 10, 20, 30, 40, 50, 60 };
         int dp[][]=new int[dims.length][dims.length];
-        System.out.println(matrixChainMultiplication(dims, 0, dims.length - 1,dp));
+        System.out.println(matrixChainMultiplicationTab(dims));
     }
 }
